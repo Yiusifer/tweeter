a85f31d3731e82332a6cccdcdd01056194ebe3c3
Adam Yiu <adam_yiu@outlook.com>
2022-05-25 22:38:20 +0000

    Creating the tweets

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
+const PORT          = 8000;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
@@ -33,7 +33,7 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 app.use("/tweets", tweetsRoutes);
 
 app.post("/tweets", (req, res) => {
-  
+
 })
 
 app.listen(PORT, () => {
